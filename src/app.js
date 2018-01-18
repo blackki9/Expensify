ef5166dc576afb9ea9a40d7e4d7784d3ce006cfc
Vyacheslav Okulov <vyacheslavokulovios@gmail.com>
2018-01-18 11:40:45 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />           
+        </div>
+
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
