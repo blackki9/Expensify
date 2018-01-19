f84c03767245a187888d3f1f8c131ebd64540cb9
Vyacheslav Okulov <vyacheslavokulovios@gmail.com>
2018-01-19 08:39:35 +0200

    Refactoring

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditExpensePage = (props) =>{
+    return (
+        <div>
+            This is from my edit expense page
+        </div>
+    );
+};
+
+export default EditExpensePage;
