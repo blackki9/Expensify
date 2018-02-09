117f5dece28533a9fc39899a780a431ce64066ee
Vyacheslav Okulov <vokulov@intersog.com>
2018-02-09 11:17:17 +0200

    Adding firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,9 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove expense action object', () => {
     const action = removeExpense({id: '123abc'});
@@ -22,38 +27,43 @@ test('should setup edit action object', () => {
 });
 
 test('should setup add expense action object with provided info', () => {
-    const expenseData = {
-        description: 'rent',
-        amount: 15000,
-        createdAt: 1000,
-        note: 'Last month rent'
-    }
+    // const expenseData = {
+    //     description: 'rent',
+    //     amount: 15000,
+    //     createdAt: 1000,
+    //     note: 'Last month rent'
+    // }
 
-    const action = addExpense(expenseData);
+    const action = addExpense(expenses[2]);
     expect(action).toEqual({
         type: 'ADD_EXPENSE',
-        expense: {
-            ...expenseData,
-            id: expect.any(String)
-        }
+        expense: expenses[2]
     })
 });
 
-test('should setup add expense action object with default values', () => {
-    const defaultSettings = {
-        description: '',
-        note: '',
-        amount: 0,
-        createdAt: 0
-      }
+test('should add expense to database and stor'e, () => {
 
-    const action = addExpense({});
+});
 
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            ...defaultSettings,
-            id: expect.any(String)
-        }
-    })
-})
+test('should add expense with defaults to database and store', () => {
+
+});
+
+// test('should setup add expense action object with default values', () => {
+//     const defaultSettings = {
+//         description: '',
+//         note: '',
+//         amount: 0,
+//         createdAt: 0
+//       }
+
+//     const action = addExpense({});
+
+//     expect(action).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             ...defaultSettings,
+//             id: expect.any(String)
+//         }
+//     })
+// })
